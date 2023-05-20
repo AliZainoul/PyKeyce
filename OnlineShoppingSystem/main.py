e5a7f6b41a7c484d871670e9c59a18f35328496d
AliZainoul <ali.zainoul.az@gmail.com>
2023-05-20 22:59:18 +0200

    Modifying files

--- OnlineShoppingSystem/main.py
+++ OnlineShoppingSystem/main.py
@@ -3,12 +3,27 @@ from src.shopping_cart import ShoppingCart
 from src.customer import Customer
 
 # Usage example
-product1 = Product(1, "Product 1", 10.99)
-product2 = Product(2, "Product 2", 19.99)
+
+products = []
+for i in range(1, 10):
+    if i % 5 == 1:
+        product = Product(i, f"Product {i}", 10.75)
+        products.append(product)
+    else:
+        product = Product(i, f"Product {i}", 5.25)
+        products.append(product)
 
 customer = Customer("John")
-customer.add_to_cart(product1)
-customer.add_to_cart(product2)
-customer.remove_from_cart(product1)
-customer.checkout()
-#customer.__cart.display_cart()  # Attempting to access the private cart attribute
+
+for product in products:
+    customer.add_to_cart(product)  # Add products to the customer's cart
+
+product1 = products[-1]
+customer.remove_from_cart(product1)  # Remove a product from the cart
+
+customer.checkout()  # Proceed to checkout
+
+
+
+# Attempting to access the private cart attribute
+# customer.__cart.display_cart()  # This line will raise an AttributeError
